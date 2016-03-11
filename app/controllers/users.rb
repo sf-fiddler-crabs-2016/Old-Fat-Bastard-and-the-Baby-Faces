ad78387edc545851d6ca494a5cb682c41e8b4c41
Todd Seller <me@toddseller.com>
2016-03-10 18:45:40 -0800

    First commit

---
+++ app/controllers/users.rb
@@ -0,0 +1,36 @@
+get '/users' do
+  @users = User.all
+  erb :'users/index'
+end
+
+get '/users/new' do
+  erb :'users/new'
+end
+
+post '/users' do
+  @user = User.create(first_name: params[:first_name], last_name: params[:last_name], user_name: params[:user_name], email: params[:email], password: params[:password])
+  if @user.valid?
+    session[:user_id] = @user.id
+    session[:name] = @user.full_name
+    redirect '/users'
+  else
+    redirect "/users/new?errors=#{@user.errors.full_messages.join(" and ")}"
+  end
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :'/users/show'
+end
+
+get '/users/:id/edit' do
+  erb :'/users/edit'
+end
+
+put '/users/:id' do
+
+end
+
+delete '/users/:id' do
+
+end
