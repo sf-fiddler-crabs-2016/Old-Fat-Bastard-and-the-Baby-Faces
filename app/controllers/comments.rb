cea90ccdcaa840ed52675d8a41ab45d1b94368b1
chrwog <wongachris@gmail.com>
2016-03-11 14:05:49 -0800

    add comment routes

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -7,3 +7,13 @@ get '/comments/:id' do
   erb :'comments/show'
 end
 
+post 'questions/:question_id/comments' do
+	@this_question = Question.find(params[:question_id])
+	@this_question.comments.create(body: params[:body], commentable_id: params[:question_id], commentable_type: 'Question', user_id: session[:user_id] )
+end
+
+
+post 'answers/:answer_id/comments' do
+	@this_answer = Answer.find(params[:answer_id])
+	@this_answer.comments.create(body: params[:body], commentable_id: params[:answer_id], commentable_type: 'Answer', user_id: session[:user_id] )
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
-  has_many :votes, as: :votable  has_many :votes, as: :votable,  dependent: :destroy
+  has_many :votes, as: :votable
+  has_many :votes, as: :votable,  dependent: :destroy
   has_many :comments, as: :commentable
   has_many :answers
   belongs_to :user
