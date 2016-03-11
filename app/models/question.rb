85ce06aa907438d9313a908302e6234f80a85a81
Todd Seller <me@toddseller.com>
2016-03-11 14:39:42 -0800

    Complete index view

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,17 @@ class Question < ActiveRecord::Base
   has_many :comments, as: :commentable
   has_many :answers
   belongs_to :user
+
+  def vote_totals
+    self.votes.count
+  end
+
+  def answer_totals
+    self.answers.count
+  end
+
+  def author
+    user = User.find(self.user_id)
+    user.user_name
+  end
 end
