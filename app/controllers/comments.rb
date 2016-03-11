a68021f77d05eccd9cd4a804b7fdef1d52794f1e
chrwog <wongachris@gmail.com>
2016-03-11 14:12:42 -0800

    Add comment routes

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,3 +1,4 @@
+# go to new comment form
 get '/comments/new' do
   erb :'comments/new'
 end
@@ -7,13 +8,16 @@ get '/comments/:id' do
   erb :'comments/show'
 end
 
+# create a comment for a question
 post 'questions/:question_id/comments' do
 	@this_question = Question.find(params[:question_id])
-	@this_question.comments.create(body: params[:body], commentable_id: params[:question_id], commentable_type: 'Question', user_id: session[:user_id] )
+	@this_question.comments.create(body: params[:body], commentable_type: 'Question', user_id: session[:user_id] )
+	# Does active record know commentable_type: 'Question' and commentable_id: params[:question_id]?
 end
 
-
+# create a comment for a answer
 post 'answers/:answer_id/comments' do
 	@this_answer = Answer.find(params[:answer_id])
-	@this_answer.comments.create(body: params[:body], commentable_id: params[:answer_id], commentable_type: 'Answer', user_id: session[:user_id] )
+	@this_answer.comments.create(body: params[:body], user_id: session[:user_id] )
+	# same as above commentable_id: params[:answer_id], commentable_type: 'Answer'
 end
